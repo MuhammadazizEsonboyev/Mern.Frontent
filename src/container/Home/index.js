8de888a052660a685537def63d2c6ed850244017
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-11-09 08:48:05 +0000

    header add

---
+++ src/container/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from '../../component/Layout'
+// import {Jombotron} from 'react-bootstrap';
+// import { Navbar, Container ,Nav } from 'react-bootstrap';
+
+
+
+
+/**
+* @author
+* @function Home
+**/
+
+export const Home = (props) => {
+  return (
+    <Layout>
+      {/* <Jombotron> */}
+      
+         <h1 className='jumbotron'>Welcome to Admin Dashboard</h1>
+      {/* </Jombotron> */}
+  
+    </Layout>
+  )
+
+}
+
