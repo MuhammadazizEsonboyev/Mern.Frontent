432fd55e7e88bd642ea99f9244b1d6b418115a52
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-11-09 16:04:04 +0000

    form-add2

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -5,6 +5,8 @@ export const login = (user) => {
     console.log(user);
 
     return async (dispatch) => {
+         
+
         dispatch({
             type: authConstants.LOGIN_REQUEST,
             payload: {
