ff4c7ddc4f00b6e4aacc4bcff19602fffb2f23f3
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-11-10 11:43:33 +0000

    form-useEffect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from './container/Home';
 import { Signin } from './container/Signin';
 import { Signup } from './container/Signup';
-// import Layout from './component/Layout/index';
+import PrivateRoute from './component/HOC/PrivateRoute'
+
 
 
 
@@ -13,7 +14,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={Home} />
+          <PrivateRoute path="/" exact component={Home} />
           <Route path="/signin" component={Signin} />
           <Route path="/signup" component={Signup} />
         </Switch>
--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -14,6 +14,7 @@ export const login = (user) => {
         if (res.status === 200) {
             const { token, user } = res.data;
             localStorage.setItem('token', token);
+            localStorage.setItem('user', JSON.stringify(user));
             dispatch({
                 type: authConstants.LOGIN_SUCCESS,
                 payload: {
@@ -31,5 +32,28 @@ export const login = (user) => {
         }
 
 
+    }
+}
+
+
+ export const isUserLoggedIn = () =>{
+    return async dispatch  => {
+        const token = localStorage.getItem('token');
+        if(token){
+            const user = JSON.parse(localStorage.getItem('user'));
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token, user
+                }
+            });
+        }else{
+            dispatch({
+                type: authConstants.LOGIN_FAILURE,
+                payload: { error: 'Failet to login ' }
+            })
+        }
+          
+     
     }
 }
